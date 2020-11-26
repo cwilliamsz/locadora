b5e1a744ec3f7d42adc9e830d3f5d5540b88bd13
Charles <cwillimsz@protonmail.com>
2020-11-25 22:21:03 -0300

    Write user into db

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
-var path = require('path');
-
-/* GET Register page. */
 router.get('/', function(req, res, next) {
   res.sendFile(__dirname + '/user.html')
+});
+
+router.post('/create', function(request, response) {
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'locadora_hands_on_work'
+  });
+
+  const user = request.body;
+  var sql = 'INSERT INTO users SET ?';
+
+  connection.query(sql, user, function (err, data) { 
+      if (err) throw err;
+         console.log("User dat is inserted successfully "); 
+         response.redirect('/user');
+  });
 });
 
 module.exports = router;
