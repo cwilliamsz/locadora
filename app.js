e1116c3d8daa0c405b166d34ce6d8d46601f2f2c
Charles <cwillimsz@protonmail.com>
2020-11-25 22:20:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -35,13 +35,12 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+  console.log(err.status, err)
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 module.exports = app;
