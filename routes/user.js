6e7a558b30fc95a81622c82d8855b50c5d09b5df
Charles <cwillimsz@protonmail.com>
2020-11-26 19:24:36 -0300

    Update log info

--- routes/user.js
+++ routes/user.js
@@ -21,9 +21,10 @@ router.post('/create', function(request, response) {
     const user = request.body
     var sql = 'INSERT INTO users SET ?'
 
-    db.query(sql, user, function (err, data) { 
+    db.query(sql, user, function (err, result) { 
         if (err) throw err
-          console.log("User data is inserted successfully ") 
+          console.log("User data is inserted successfully ")
+          console.log(result)
           response.redirect('/user')
     })
   } else {
@@ -37,9 +38,10 @@ router.get('/delete/:id', function(request, response) {
     const userId = request.params.id
     var sql = 'DELETE FROM users WHERE id = ?'
 
-    db.query(sql, userId, function (err, data) { 
+    db.query(sql, userId, function (err, result) { 
         if (err) throw err
-          console.log("User data is deleted successfully ") 
+          console.log("User data is deleted successfully ")
+          console.log(result)
           response.redirect('/user')
     })
   } else {
