e0651dfad2e47ffe2ab1f2579bde1ee1d9628ef5
Charles <cwillimsz@protonmail.com>
2020-11-25 22:47:23 -0300

    Refactor session key's, remove ";"

--- app.js
+++ app.js
@@ -1,46 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
-var bodyParser = require('body-parser');
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+var session = require('express-session')
+var bodyParser = require('body-parser')
+
+const sessionConfig = require('./config/session')
 
 // Routes
-var userRouter = require('./routes/user');
-var loginRouter = require('./routes/login');
-var homeRouter = require('./routes/home');
-var reservationRouter = require('./routes/reservation');
+var userRouter = require('./routes/user')
+var loginRouter = require('./routes/login')
+var homeRouter = require('./routes/home')
+var reservationRouter = require('./routes/reservation')
 
-var app = express();
+var app = express()
 
 app.use(session({
-    secret: 'fjweiufh93ghbfierifghiebfbowebbfuiuiwrwbno7998re',
-    resave: true,
-    saveUninitialized: true
-}));
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
+    secret: sessionConfig.secret,
+    resave: sessionConfig.resave,
+    saveUninitialized: sessionConfig.saveUninitialized
+}))
+
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.json())
 
 // Endpoints
-app.use('/', loginRouter);
-app.use('/user', userRouter);
-app.use('/home', homeRouter);
-app.use('/reservation', reservationRouter);
+app.use('/', loginRouter)
+app.use('/user', userRouter)
+app.use('/home', homeRouter)
+app.use('/reservation', reservationRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   console.log(err.status, err)
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.json({
     message: err.message,
     error: err
-  });
-});
+  })
+})
 
-module.exports = app;
+module.exports = app
---
+++ config/session.js
@@ -0,0 +1,6 @@
+sessionConfig = { 
+    secret: '$fgfhghgfkk0i93490031sdsd1!@#$HnwTKJRtCTtHqIoR/8gqCRRcpDgC',
+    resave: true,
+    saveUninitialized: true
+} 
+module.exports = sessionConfig;
