82b6630a3bc6a08888e08ffe89c67476a39e4da9
Charles <cwillimsz@protonmail.com>
2020-11-28 12:26:34 -0300

    Wip Car load color from database

--- routes/car.js
+++ routes/car.js
@@ -1,9 +1,20 @@
 var express = require('express')
 var router = express.Router()
-var path = require('path')
 
+const db = require('../config/connection')
+
+// Get
 router.get('/', function(request, response, next) {
-  response.sendFile(path.join(__dirname, '../views', 'car.html'))
+  if (request.session.user) {
+    var sql='SELECT * FROM cars';
+    db.query(sql, function (err, data, fields) {
+      if (err) throw err;
+      response.render('car', { title: 'Car List', data: data});
+    });
+  } else {
+    request.session.error = 'Access denied!'
+    response.redirect('/')
+  }
 })
 
 module.exports = router
