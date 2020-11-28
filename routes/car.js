c930397ed467e44c1bb5247444195dc23b8e3dee
Charles <cwillimsz@protonmail.com>
2020-11-28 12:37:33 -0300

    Add load database data to select menu in html

--- routes/car.js
+++ routes/car.js
@@ -7,9 +7,16 @@ const db = require('../config/connection')
 router.get('/', function(request, response, next) {
   if (request.session.user) {
     var sql='SELECT * FROM cars';
-    db.query(sql, function (err, data, fields) {
+    db.query(sql, function (err, cars, fields) {
       if (err) throw err;
-      response.render('car', { title: 'Car List', data: data});
+      var sqlColors='SELECT * FROM colors';
+      db.query(sqlColors, function (err, colors, fields) {
+        if (err) throw err;
+        response.render('car',  { title: 'Car List', 
+                                  data: cars,
+                                  colors: colors
+                                });
+      });
     });
   } else {
     request.session.error = 'Access denied!'
